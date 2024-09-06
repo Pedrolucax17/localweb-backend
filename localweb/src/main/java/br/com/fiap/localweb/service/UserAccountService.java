a006d6c1bfa243bea4b40b2e2cd62d614a7e99fc
Pedro Lucas <pl.lucassouza@gmail.com>
2024-09-06 15:16:30 -0300

    Implementing the initial crud in the userAccountService class

--- localweb/src/main/java/br/com/fiap/localweb/service/UserAccountService.java
+++ localweb/src/main/java/br/com/fiap/localweb/service/UserAccountService.java
@@ -9,4 +9,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserAccountService {
 
+    @Autowired
+    private UserAccountRepository accountRepository;
+
+    public UserAccount saveUserAccount(UserAccount userAccount){
+        return accountRepository.save(userAccount);
+    }
+
+    public List<UserAccount> listAllUserAccount(){
+        return accountRepository.findAll();
+    }
+
+    public UserAccount updateUserAccount(UserAccount userAccount){
+        Optional<UserAccount> accountOptional = accountRepository.findById(userAccount.getId());
+        if(accountOptional.isPresent()){
+            return accountRepository.save(userAccount);
+        }else{
+            throw new RuntimeException("Contato não encontrado");
+        }
+    }
+
+    public void deleteUserAccount(Long id){
+        Optional<UserAccount> accountOptional = accountRepository.findById(id);
+        if (accountOptional.isPresent()){
+            accountRepository.deleteById(id);
+        }else{
+            throw new RuntimeException("Contato não encontrado!");
+        }
+
+    }
+
 }
