c2b8be6e8d4a87521611ba19c79c068346b10a62
Pedro Lucas <pl.lucassouza@gmail.com>
2024-09-10 19:02:17 -0300

    Modifying user preferences

---
+++ localweb/src/main/java/br/com/fiap/localweb/model/Category.java
@@ -0,0 +1,8 @@
+package br.com.fiap.localweb.model;
+
+public enum Category {
+    PESSOAL,
+    TRABALHO,
+    SOCIAL,
+    PROMOCAO
+}
--- localweb/src/main/java/br/com/fiap/localweb/model/UserPreferences.java
+++ localweb/src/main/java/br/com/fiap/localweb/model/UserPreferences.java
@@ -3,5 +3,7 @@ package br.com.fiap.localweb.model;
 public class UserPreferences {
 
     private Theme theme;
+    private String color;
+    private Category category;
 
 }
