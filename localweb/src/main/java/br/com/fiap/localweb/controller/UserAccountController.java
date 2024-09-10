3f5dc588bc0fd3fdeb73c8b8ed950b363c9649fc
Pedro Lucas <pl.lucassouza@gmail.com>
2024-09-10 17:25:48 -0300

    Creating basic crud for the user account

--- localweb/src/main/java/br/com/fiap/localweb/controller/UserAccountController.java
+++ localweb/src/main/java/br/com/fiap/localweb/controller/UserAccountController.java
@@ -1,13 +1,32 @@
 package br.com.fiap.localweb.controller;
 
+import br.com.fiap.localweb.model.UserAccount;
 import br.com.fiap.localweb.repository.UserAccountRepository;
 import br.com.fiap.localweb.service.UserAccountService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/account")
 public class UserAccountController {
+
+    @Autowired
+    private UserAccountService userAccountService;
+
+    @PostMapping
+    public UserAccount saveUserAccount(UserAccount userAccount){
+        return userAccountService.saveUserAccount(userAccount);
+    }
+
+    @GetMapping
+    public List<UserAccount> listAllUserAccount(){
+        return userAccountService.listAllUserAccount();
+    }
+
+    @DeleteMapping
+    public void deleteUserAccount(Long id){
+        userAccountService.deleteUserAccount(id);
+    }
 
 }
--- localweb/src/main/java/br/com/fiap/localweb/model/UserPreferences.java
+++ localweb/src/main/java/br/com/fiap/localweb/model/UserPreferences.java
@@ -1,4 +1,7 @@
 package br.com.fiap.localweb.model;
 
 public class UserPreferences {
+
+    private Theme theme;
+
 }
