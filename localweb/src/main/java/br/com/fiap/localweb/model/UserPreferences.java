901de95088a8543da3745deae070346f15b8b4f8
Pedro Lucas <pl.lucassouza@gmail.com>
2024-09-11 16:37:41 -0300

    using flyway to create the user account table

--- localweb/src/main/java/br/com/fiap/localweb/model/UserPreferences.java
+++ localweb/src/main/java/br/com/fiap/localweb/model/UserPreferences.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class UserPreferences {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne
