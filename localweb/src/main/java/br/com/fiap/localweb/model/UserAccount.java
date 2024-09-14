34d47ba900659aded8fb99fc7f0c7570d39234a2
Pedro Lucas <pl.lucassouza@gmail.com>
2024-09-14 09:00:17 -0300

    Implementing validations

--- localweb/src/main/java/br/com/fiap/localweb/model/UserAccount.java
+++ localweb/src/main/java/br/com/fiap/localweb/model/UserAccount.java
@@ -3,6 +3,7 @@ package br.com.fiap.localweb.model;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
@@ -19,15 +20,15 @@ public class UserAccount {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull(message = "O nome é obrigatório")
+    @NotBlank(message = "O nome é obrigatório")
     private String name;
 
     @Column(unique = true)
-    @NotNull(message = "O email é obrigatório")
+    @NotBlank(message = "O email é obrigatório")
     @Email(message = "O formato do email está inválido")
     private String email;
 
-    @NotNull(message = "A senha é obrigatória")
+    @NotBlank(message = "A senha é obrigatória")
     private String password;
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
