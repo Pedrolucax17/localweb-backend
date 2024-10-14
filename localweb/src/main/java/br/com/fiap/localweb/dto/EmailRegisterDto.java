74e0865168d047a7f89bb229d106f8ea2455255a
Pedro Lucas <pl.lucassouza@gmail.com>
2024-10-14 16:46:22 -0300

    fixing isRead attribute validation problem

--- localweb/src/main/java/br/com/fiap/localweb/dto/EmailRegisterDto.java
+++ localweb/src/main/java/br/com/fiap/localweb/dto/EmailRegisterDto.java
@@ -2,6 +2,7 @@ package br.com.fiap.localweb.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record EmailRegisterDto(
         @NotBlank(message = "O destinatário é obrigatório")
@@ -14,7 +15,7 @@ public record EmailRegisterDto(
         String subject,
         @NotBlank(message = "O conteúdo é obrigatório")
         String content,
-        @NotBlank(message = "O campo isRead é obrigatório")
+        @NotNull(message = "O campo isRead é obrigatório")
         Boolean isRead
 ) {
 }
